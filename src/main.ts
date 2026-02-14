b055af6dcec867c1af53af7786677397187e2463
Sailaja <durbha.sailaja@skill-mine.com>
2026-02-14 23:44:35 +0530

    frontend domain change

--- src/main.ts
+++ src/main.ts
@@ -9,7 +9,7 @@ async function bootstrap() {
   app.enableCors({
     origin: [
       "http://localhost:3001",
-      "https://user-login-frontend.vercel.app",
+      "https://user-login-frontend-mpef.vercel.app",
     ],
     credentials: true,
   });
